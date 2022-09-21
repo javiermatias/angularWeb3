de3317ad39bf7222044d60dbb6fd52620ecffe14
JAVIER MATIAS <javierjimenez78@gmail.com>
2022-09-21 03:20:00 -0300

    sign functionality

--- src/app/components/services/wallet.service.ts
+++ src/app/components/services/wallet.service.ts
@@ -1,8 +1,8 @@
-import { EnvironmentInjector, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs'
 import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
-import { ethers } from "ethers";
+import { MsgParam } from './msg-param';
 import Swal from 'sweetalert2';
 
 declare let window: any;
@@ -20,13 +20,15 @@ export class WalletService {
   configUrl = 'assets/simple-abi.json';
   contractABI: string = '';
   ethereum: any = null;
- 
+  params: MsgParam;
+
 
 
   constructor(private http: HttpClient) {
-    if(this.existsMetamask()){
-      console.log("Hay Billetera");
-    }else{
+    this.params = new MsgParam();
+    if (this.existsMetamask()) {
+      this.listenChangeUser();// we listen if the user connected other account
+    } else {
       Swal.fire(
         {
           icon: 'error',
@@ -52,38 +54,58 @@ export class WalletService {
   }
 
   async connect() {
-    if (this.existsMetamask()) {
+    if (this.existsMetamask() && await this.chainNetwork()) {
       try {
-        await this.chainNetwork();
         const account = await this.ethereum.request({
           method: 'eth_requestAccounts',
         });
-
         this.userAddress.next(account[0]);
-        this.listenChangeUser();
-      } catch (error) {
-        console.error(error);
-        throw error;
+      } catch (error: any) {
+        if (error.code == 4902) {
+          Swal.fire(
+            {
+              icon: 'error',
+              title: 'Opss..',
+              text: 'Qatar prode funciona sobre la red de Polygon(matic), por favor instala esa red. Mas abajo veras las instruccioens.'
+            }
+          )
+        } else {
+          Swal.fire(
+            {
+              icon: 'error',
+              title: 'Opss..',
+              text: 'Ocurrio un error por favor intenta de vuelta '
+            }
+          )
+        }
+        //loguear error
       }
     }
 
 
   }
 
-  async chainNetwork() {
+  async chainNetwork(): Promise<boolean> {
     try {
       await this.ethereum.request({
         method: 'wallet_switchEthereumChain',
         params: [{ chainId: environment.chainId }], //0x1 etherum mainet, 0x2 goerli, 0x4 etherum Rinkbey
       });
+      return true;
     } catch (error: any) {
-      
       if (error.code == 4902) {
-        //await this.addChain();
-      }else{
-          throw error;
+        Swal.fire(
+          {
+            icon: 'error',
+            title: 'Opss..',
+            text: 'Qatar prode funciona sobre la red de Polygon(matic), por favor instala esa red. Mas abajo veras las instruccioens.'
+          }
+        )
       }
-      
+      console.log(error.message);
+      return false;
+
+
     }
   }
 
@@ -106,7 +128,7 @@ export class WalletService {
       ],
     });
   }
-  isConnected(): boolean {    
+  isConnected(): boolean {
     return this.ethereum.selectedAddress ? true : false;
   }
   getAddressConnnected(): string | undefined {
@@ -114,13 +136,31 @@ export class WalletService {
     return this.ethereum.selectedAddress;
   }
 
-  listenChangeUser(){
-    this.ethereum.on('accountsChanged', 
-    (account:any) => {
-      this.userAddress.next(account[0]);
-    });
+  listenChangeUser() {
+    this.ethereum.on('accountsChanged',
+      (account: any) => {
+        this.userAddress.next(account[0]);
+        console.log(account[0]);
+      });
   }
 
+
+  async signData(_nonce: string) {
+    try {
+      const from = this.getAddressConnnected();
+      const sign = await this.ethereum.request({
+        method: 'eth_signTypedData_v4',
+        params: [from, JSON.stringify(this.params.getMsgParam(_nonce))],
+      });
+      console.log(sign)
+
+    } catch (err) {
+      console.error(err);
+      // signTypedDataV4Result.innerHTML = `Error: ${err.message}`;
+    }
+  };
+
+}
 
 
 /*   emitAddressConnected() {
@@ -131,4 +171,4 @@ export class WalletService {
     }
   } */
 
-}
+
